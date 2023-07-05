3817fb0271b4f68707a27dbdb36dff4f1a25bca7
adamE38 <akwork63@gmail.com>
2023-07-05 13:23:14 +0200

    set up html, css, some js

---
+++ 002-modal/js/app.js
@@ -0,0 +1,5 @@
+const openModalBtn = document.querySelector('.open')
+const closeModalBtn = document.querySelector('.close')
+const modal = document.querySelector('#moda;')
+const overlay = document.querySelector('#overlay')
+
