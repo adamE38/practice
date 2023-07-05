f926ced8654c96dabae6025265aa9582ea670177
adamE38 <akwork63@gmail.com>
2023-07-05 13:28:20 +0200

    refactored js

--- 002-modal/js/app.js
+++ 002-modal/js/app.js
@@ -2,18 +2,24 @@ const openModalBtn = document.querySelector('.open')
 const closeModalBtn = document.querySelector('.close')
 const modal = document.querySelector('#modal')
 const overlay = document.querySelector('#overlay')
+const closingElements = [closeModalBtn, overlay]
 
 openModalBtn.addEventListener('click', () => {
+    openModal()
+})
+
+closingElements.forEach(element => {
+    element.addEventListener('click', () => {
+        closeModal()
+    })
+})
+
+function openModal() {
     modal.classList.add('visible')
     overlay.classList.add('visible')
-})
+}
 
-closeModalBtn.addEventListener('click', () => {
+function closeModal() {
     modal.classList.remove('visible')
     overlay.classList.remove('visible')
-})
-
-overlay.addEventListener('click', () => {
-    modal.classList.remove('visible')
-    overlay.classList.remove('visible')
-})
+}
