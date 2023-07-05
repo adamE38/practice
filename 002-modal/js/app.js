8d73e85a308b0386c5be465a8f2d4c7adcae61ee
adamE38 <akwork63@gmail.com>
2023-07-05 13:25:45 +0200

    completed js

--- 002-modal/js/app.js
+++ 002-modal/js/app.js
@@ -1,5 +1,19 @@
 const openModalBtn = document.querySelector('.open')
 const closeModalBtn = document.querySelector('.close')
-const modal = document.querySelector('#moda;')
+const modal = document.querySelector('#modal')
 const overlay = document.querySelector('#overlay')
 
+openModalBtn.addEventListener('click', () => {
+    modal.classList.add('visible')
+    overlay.classList.add('visible')
+})
+
+closeModalBtn.addEventListener('click', () => {
+    modal.classList.remove('visible')
+    overlay.classList.remove('visible')
+})
+
+overlay.addEventListener('click', () => {
+    modal.classList.remove('visible')
+    overlay.classList.remove('visible')
+})
